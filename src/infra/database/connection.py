6aee1bde8ae11e8130647b5cd3f21985710a88ca
Levy Araujo <lev0x79@gmail.com>
2024-09-23 23:19:04 -0300

    cria repo de escrita e leitura

--- src/infra/database/connection.py
+++ src/infra/database/connection.py
@@ -1,17 +1,20 @@
 from os import getenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+metadata = MetaData()
 
-def get_db():
-    session = SessionLocal()
+def get_session():
+    session = Session()
     try:
         yield session
     finally:
