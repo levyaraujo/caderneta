3e709a5670334bd25150e72c8e8c77be7fc4fcf4
Levy Araujo <lev0x79@gmail.com>
2024-09-23 23:27:04 -0300

    adiciona TipoTransacaoORM

--- src/dominio/transacao/tipos.py
+++ src/dominio/transacao/tipos.py
@@ -1,5 +1,21 @@
 from enum import Enum
 
+from sqlalchemy.types import TypeDecorator, String
+
+
+class TipoTransacaoORM(TypeDecorator):
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, TipoTransacao):
+            return value.value
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return TipoTransacao(value)
+        return value
+
 
 class TipoTransacao(Enum):
     DEBITO = "debito"
