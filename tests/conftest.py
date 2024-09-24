541517313f69fff933a998d46221ade2b9d2c50f
Levy Araujo <lev0x79@gmail.com>
2024-09-23 23:23:29 -0300

    organize fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,13 @@ import pytest
 
 from src.dominio.transacao.entidade import Transacao
 from src.dominio.usuario.entidade import Usuario
+from src.infra.database.orm import iniciar_mapeamento_orm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.infra.database.connection import metadata
+from src.infra.database.repo import RepoEscrita, RepoLeitura
+
+iniciar_mapeamento_orm()
 
 
 @pytest.fixture
@@ -18,9 +25,9 @@ def usuario():
         senha="senha123"
     )
 
+
 @pytest.fixture(scope="function")
 def transacao_gen():
-
     def make_mock(usuario, valor, destino, tipo, caixa=datetime(2024, 10, 22)):
         return Transacao(
             id=randint(1, 1000),
@@ -33,3 +40,36 @@ def transacao_gen():
         )
 
     return make_mock
+
+
+DATABASE_URL = "sqlite:///:memory:"
+
+
+@pytest.fixture(scope="module")
+def engine():
+    return create_engine(DATABASE_URL, echo=True)
+
+
+@pytest.fixture(scope="module")
+def tables(engine):
+    metadata.create_all(engine)
+    yield
+    metadata.drop_all(engine)
+
+
+@pytest.fixture
+def session(engine, tables):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
+    session.close()
+
+
+@pytest.fixture
+def repo_escrita(session):
+    return RepoEscrita(session)
+
+
+@pytest.fixture
+def repo_leitura(session):
+    return RepoLeitura(session)
