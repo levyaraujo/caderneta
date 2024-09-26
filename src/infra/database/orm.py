1e5168e98b2510f8bacc7d74c7e8f789d4b0c150
Levy Araujo <lev0x79@gmail.com>
2024-09-25 21:47:04 -0300

    [Refactor][Infra][DB] Refatora mapeamento
    * refactor: refatora mapeamento da entidade para modelo sqlalchemy

--- src/infra/database/orm.py
+++ src/infra/database/orm.py
@@ -1,11 +1,20 @@
-from sqlalchemy import Table, Column, Integer, Float, String, DateTime, ForeignKey
-from sqlalchemy.orm import registry
+from sqlalchemy import (
+    Table,
+    Column,
+    Integer,
+    Float,
+    String,
+    DateTime,
+    ForeignKey,
+    Boolean,
+)
+from sqlalchemy.orm import registry, relationship
 from src.dominio.transacao.entidade import Transacao
 from src.dominio.transacao.tipos import TipoTransacaoORM
 from src.dominio.usuario.entidade import Usuario
 from src.infra.database.connection import metadata
 
-mapper = registry()
+mapper = registry(metadata=metadata)
 
 usuarios = Table(
     "usuarios",
@@ -28,9 +37,25 @@ transacoes = Table(
     Column("tipo", TipoTransacaoORM),
     Column("descricao", String),
     Column("caixa", DateTime),
+    Column("competencia", DateTime),
+    Column("apagado", Boolean, default=False),
 )
 
 
 def iniciar_mapeamento_orm():
-    mapper.map_imperatively(Usuario, usuarios)
-    mapper.map_imperatively(Transacao, transacoes)
+    mapper.map_imperatively(
+        Usuario,
+        usuarios,
+        properties={"transacoes": relationship(Transacao, back_populates="usuario")},
+    )
+    mapper.map_imperatively(
+        Transacao,
+        transacoes,
+        properties={
+            "usuario": relationship(Usuario, back_populates="transacoes"),
+            "usuario_id": transacoes.c.usuario_id,
+        },
+    )
+
+
+iniciar_mapeamento_orm()
