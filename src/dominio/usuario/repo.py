595d66baff018d4a533f8c78c83c4e0b5ce1b412
Levy Araujo <lev0x79@gmail.com>
2024-09-28 20:09:50 -0300

    adiciona testes github actions

---
+++ src/dominio/usuario/repo.py
@@ -0,0 +1,20 @@
+from src.dominio.usuario.entidade import Usuario
+from src.infra.database.repo import RepoLeitura
+
+
+class RepoUsuarioLeitura(RepoLeitura):
+    def buscar_por_email(self, email: str):
+        return self.session.query(Usuario).filter(Usuario.email == email).first()
+
+    def buscar_por_telefone(self, telefone: str):
+        return self.session.query(Usuario).filter(Usuario.telefone == telefone).first()
+
+    def buscar_por_id(self, id: int):
+        return self.session.query(Usuario).filter(Usuario.id == id).first()
+
+    def buscar_por_email_e_senha(self, email: str, senha: str):
+        return (
+            self.session.query(Usuario)
+            .filter(Usuario.email == email, Usuario.senha == senha)
+            .first()
+        )
