d5b52a652e814769e881dc18840e849c327dc375
Levy <lev0x79@gmail.com>
2024-10-19 23:46:02 -0300

    altera serviço do bot para treinar o modelo

--- src/dominio/bot/services.py
+++ src/dominio/bot/services.py
@@ -1,6 +1,15 @@
 from src.dominio.bot.entidade import TwilioBot
+from src.dominio.processamento.entidade import TextClassifier
+import joblib
 
 
-def responder_usuario(mensagem: str):
+def responder_usuario(mensagem: str, usuario: str):
     bot = TwilioBot()
-    return bot.responder(mensagem)
+    classifier = TextClassifier()
+    tipo, prob = classifier.classify_message(mensagem)
+    probabilidades = ""
+    for t, p in prob.items():
+        probabilidades += f"{t}: {p}\n"
+
+    resposta = f"O tipo da mensagem é: {tipo} com as probabilidades: \n{probabilidades}"
+    return bot.responder(resposta, usuario)
