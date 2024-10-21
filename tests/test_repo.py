49a7a125bb3d3d8651049730ba4ed38caa70ce6e
Levy <lev0x79@gmail.com>
2024-10-21 20:07:59 -0300

    corrige docker de teste e produção e implementa uow

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -6,64 +6,66 @@ from src.dominio.usuario.entidade import Usuario
 from src.dominio.transacao.entidade import Transacao
 from src.dominio.transacao.tipos import TipoTransacao
 from src.dominio.usuario.repo import RepoUsuarioLeitura
+from src.infra.database.uow import UnitOfWork
 from src.libs.tipos import Intervalo
+from tests.conftest import repo_escrita
 
 
-def test_adicionar_usuario(repo_escrita, session):
-    usuario = Usuario(
-        id=1,
-        nome="John",
-        sobrenome="Doe",
-        telefone="94981360000",
-        email="john@example.com",
-        senha="password",
-    )
-    repo_escrita.adicionar(usuario)
-    repo_escrita.commit()
+def test_adicionar_usuario(session):
+    uow = UnitOfWork(session_factory=lambda: session)
+    with uow:
+        usuario = Usuario(
+            id=1,
+            nome="John",
+            sobrenome="Doe",
+            telefone="94981360000",
+            email="john@example.com",
+            senha="password",
+        )
+        uow.repo_escrita.adicionar(usuario)
+        uow.commit()
     assert session.query(Usuario).filter_by(id=1).first() is not None
 
 
-def test_remover_usuario(repo_escrita, session):
-    usuario = session.query(Usuario).filter_by(id=1).first()
-    repo_escrita.remover(usuario)
-    repo_escrita.commit()
-    assert session.query(Usuario).filter_by(id=1).first() is None
+def test_remover_usuario(session, mock_usuario):
+    usuario = mock_usuario
+    uow = UnitOfWork(session_factory=lambda: session)
+    with uow:
+        usuario = session.query(Usuario).filter_by(id=usuario.id).first()
+        uow.repo_escrita.remover(usuario)
+        uow.commit()
+    assert session.query(Usuario).filter_by(id=usuario.id).first() is None
 
 
-def test_adicionar_transacao(repo_escrita, session):
-    usuario = Usuario(
-        id=randint(1, 10000),
-        nome="Jane",
-        sobrenome="Doe",
-        telefone="94981360000",
-        email="jane@example.com",
-        senha="password",
-    )
-    session.add(usuario)
-    session.commit()
+def test_adicionar_transacao(session, mock_usuario):
+    uow = UnitOfWork(session_factory=lambda: session)
+    repo_transacao_leitura = RepoTransacaoLeitura(session=session)
+    usuario = mock_usuario
     transacao = Transacao(
         id=randint(1, 10000),
         usuario=usuario,
         valor=100.0,
         tipo=TipoTransacao.CREDITO,
-        categoria=None,
+        categoria="roupas",
         descricao="Test",
         caixa=datetime.now(),
     )
-    repo_escrita.adicionar(transacao)
-    repo_escrita.commit()
-    assert session.query(Transacao).filter_by(id=transacao.id).first() is not None
+    with uow:
+        uow.repo_escrita.adicionar(usuario)
+        uow.repo_escrita.adicionar(transacao)
+        uow.commit()
+
+    assert repo_transacao_leitura.buscar_por_id(entidade=transacao) is not None
 
 
 def test_buscar_usuario_por_id(session, mock_usuario):
-    repo = RepoUsuarioLeitura(session=session)
     novo_usuario = mock_usuario
-    session.add(novo_usuario)
-    session.commit()
 
-    usuario = repo.buscar_por_id(novo_usuario.id)
+    repo_usuario_leitura = RepoUsuarioLeitura(session=session)
+
+    usuario = repo_usuario_leitura.buscar_por_id(novo_usuario.id)
     assert usuario is not None
-    assert usuario.nome == "Joao"
+    assert usuario.nome == "Usuario"
 
 
 def test_buscar_todas_transacoes(repo_leitura):
@@ -73,18 +75,13 @@ def test_buscar_todas_transacoes(repo_leitura):
 
 def test_buscar_transacao_por_intervalo_e_usuario(session, mock_usuario, transacao_gen):
     repo_transacao_leitura = RepoTransacaoLeitura(session=session)
+    uow = UnitOfWork(session_factory=lambda: session)
     usuario = mock_usuario
-    for _ in range(10):
-        session.add(
-            transacao_gen(
-                usuario=usuario,
-                valor=randint(150, 2000),
-                caixa=datetime(2024, randint(9, 10), randint(1, 30)),
-                destino="Loja A",
-                tipo=TipoTransacao.CREDITO,
-            )
-        )
-    session.commit()
+    with uow:
+        for _ in range(10):
+            transacao = transacao_gen(usuario, 100.0, "Loja A", TipoTransacao.CREDITO)
+            uow.repo_escrita.adicionar(transacao)
+            uow.commit()
 
     intervalo = Intervalo(inicio=datetime(2024, 9, 1), fim=datetime(2024, 10, 30))
     transacoes = repo_transacao_leitura.buscar_por_intervalo_e_usuario(
