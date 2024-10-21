6bd486743411b953db968b6a65315dc556ca4eac
Levy <lev0x79@gmail.com>
2024-10-21 20:12:43 -0300

    melhora conexão com o banco

--- src/infra/database/connection.py
+++ src/infra/database/connection.py
@@ -1,26 +1,28 @@
 from contextlib import contextmanager
-from os import getenv
+from os import getenv, path
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from dotenv import load_dotenv
 
-load_dotenv()
+env = getenv("ENV", "development")
+dotenv_path = "tests/.env" if env == "testing" else ".env"
+if path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
-DATABASE_URL = getenv("DATABASE_URL", "sqlite:///:memory:")
+DATABASE_URL = getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 metadata = MetaData()
 
 
 @contextmanager
-def get_session():
+def GET_DEFAULT_SESSION():
     session = Session()
     try:
         yield session
     except Exception as e:
-        session.rollback()
         raise e
     finally:
         session.close()
