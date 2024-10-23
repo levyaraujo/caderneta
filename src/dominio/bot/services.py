6daf11857826cafbad3c58cd1deb875dd342cba2
Levy <lev0x79@gmail.com>
2024-10-22 22:56:36 -0300

    traduz classes e melhora resposta do bot

--- src/dominio/bot/services.py
+++ src/dominio/bot/services.py
@@ -1,18 +1,18 @@
-from src.dominio.bot.entidade import TwilioBot, BotBase
-from src.dominio.processamento.entidade import TextClassifier, FinancialMessageParser
+from src.dominio.bot.entidade import BotBase
+from src.dominio.processamento.entidade import ClassificadorTexto, ConstrutorTransacao
+from src.dominio.transacao.entidade import Real
 from src.dominio.transacao.tipos import TipoTransacao
 
 
 def responder_usuario(mensagem: str, usuario: str, bot: BotBase):
-    classifier = TextClassifier()
-    tipo, prob = classifier.classify_message(mensagem)
-    tipo = str(tipo).upper()
-    parser = FinancialMessageParser(acao=TipoTransacao[tipo])
+    classifier = ClassificadorTexto()
+    tipo, prob = classifier.classificar_mensagem(mensagem)
+    parser = ConstrutorTransacao(acao=TipoTransacao[tipo])
+    acao = "pagamento" if tipo == "DEBITO" else "recebimento"
     transacao = parser.parse_message(mensagem)
-    print(parser.format_transaction(transacao))
     probabilidades = ""
     for t, p in prob.items():
         probabilidades += f"{t}: {p}\n"
 
-    resposta = f"O tipo da mensagem é: {tipo} com as probabilidades: \n{probabilidades}"
+    resposta = f"Entendi! Houve um {acao} de {Real(transacao.valor)} no dia {transacao.data_formatada} na categoria *{transacao.categoria}*."
     return bot.responder(resposta, usuario)
