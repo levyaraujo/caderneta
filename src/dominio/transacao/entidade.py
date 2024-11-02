d23d63974b8eaebfcaa55f6b1acb0362a233ef71
Levy <lev0x79@gmail.com>
2024-11-02 02:13:48 -0300

    corrige processo de criação de transação e tipagem

--- src/dominio/transacao/entidade.py
+++ src/dominio/transacao/entidade.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import datetime, date
 
 from src.dominio.transacao.tipos import TipoTransacao
 from src.dominio.usuario.entidade import Usuario
-from src.validadores import validar_tipo_da_transacao
+from src.utils.validadores import validar_tipo_da_transacao
 
 
 @dataclass(frozen=False)
@@ -14,7 +14,7 @@ class Transacao:
     id: int | None = field(default=None)
     categoria: str | None = field(default=None)
     descricao: str | None = field(default=None)
-    caixa: datetime = field(default_factory=datetime.now)
+    caixa: datetime | date = field(default_factory=datetime.now)
     competencia: datetime = field(default_factory=datetime.now)
     apagado: bool = False
 
--- src/dominio/transacao/services.py
+++ src/dominio/transacao/services.py
@@ -1,21 +1,23 @@
 import logging
 from typing import List
 
+from src.dominio.processamento.entidade import ConstrutorTransacao
 from src.dominio.transacao.exceptions import ErroAoCriarTransacao
 from src.dominio.transacao.tipos import TipoTransacao
+from src.dominio.usuario.entidade import Usuario
 from src.infra.database.uow import UnitOfWork
 from src.libs.tipos import Intervalo
-from src.dominio.transacao.entidade import Transacao
+from src.dominio.transacao.entidade import Transacao, Real
 
 logger = logging.getLogger("transacao.servicos")
 
 
-def _calcular_caixa(transacoes: List[Transacao], intervalo: Intervalo):
+def _calcular_caixa(transacoes: List[Transacao], intervalo: Intervalo) -> float:
     transacoes_no_periodo = [
         transacao for transacao in transacoes if intervalo.contem(transacao.caixa)
     ]
 
-    soma = 0
+    soma = 0.0
 
     for transacao in transacoes_no_periodo:
         soma += (
@@ -27,12 +29,12 @@ def _calcular_caixa(transacoes: List[Transacao], intervalo: Intervalo):
     return soma
 
 
-def _calcular_competencia(transacoes: List[Transacao], intervalo: Intervalo):
+def _calcular_competencia(transacoes: List[Transacao], intervalo: Intervalo) -> float:
     transacoes_no_periodo = [
         transacao for transacao in transacoes if intervalo.contem(transacao.caixa)
     ]
 
-    soma = 0
+    soma = 0.0
 
     for transacao in transacoes_no_periodo:
         soma += (
@@ -44,13 +46,35 @@ def _calcular_competencia(transacoes: List[Transacao], intervalo: Intervalo):
     return soma
 
 
-def criar_transacao(transacao: Transacao, uow: UnitOfWork):
+def salvar_transacao(transacao: Transacao, uow: UnitOfWork):
     try:
         with uow:
             uow.repo_escrita.adicionar(transacao)
             uow.commit()
     except Exception as e:
-        logger.error(f"Erro ao criar transacao: {e}")
-        raise ErroAoCriarTransacao(
+        logger.error(
             f"Erro ao criar transação para o usuario {transacao.usuario.id}: {e}"
         )
+        raise ErroAoCriarTransacao(f"Erro ao criar transação.")
+
+
+def comando_criar_transacao(
+    usuario: Usuario, tipo: str, mensagem: str, uow: UnitOfWork
+) -> str:
+    parser = ConstrutorTransacao(acao=TipoTransacao[tipo])
+    acao = "pagamento" if tipo == "DEBITO" else "recebimento"
+    transacao_comando = parser.parse_message(mensagem)
+    transacao = Transacao(
+        usuario=usuario,
+        valor=transacao_comando.valor,
+        tipo=transacao_comando.tipo,
+        categoria=transacao_comando.categoria,
+        caixa=transacao_comando.data,
+    )
+    salvar_transacao(transacao=transacao, uow=uow)
+    resposta = (
+        f"Entendi! Houve um {acao} de {Real(transacao_comando.valor)} no dia {transacao_comando.data_formatada} "
+        f"na categoria *{transacao_comando.categoria}*."
+    )
+
+    return resposta
