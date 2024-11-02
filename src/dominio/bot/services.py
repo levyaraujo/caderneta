cb277076961d4827de791c1ac83cdc5c4fc49526
Levy <lev0x79@gmail.com>
2024-11-02 02:01:07 -0300

    implementa pequenas melhorias nos serviços do bot

--- src/dominio/bot/services.py
+++ src/dominio/bot/services.py
@@ -1,36 +1,35 @@
+from src.dominio.bot.comandos import bot
 from src.dominio.bot.entidade import BotBase
 from src.dominio.bot.exceptions import ComandoDesconhecido
-from src.dominio.processamento.entidade import ClassificadorTexto, ConstrutorTransacao
+from src.dominio.processamento.entidade import ClassificadorTexto
 from src.dominio.processamento.exceptions import NaoEhTransacao
-from src.dominio.transacao.entidade import Real
-from src.dominio.transacao.tipos import TipoTransacao
-from src.dominio.bot.comandos import bot
+from src.dominio.transacao.services import comando_criar_transacao
+from src.dominio.usuario.entidade import Usuario
+from src.infra.database.uow import UnitOfWork
 
 
 async def responder_usuario(
-    mensagem: str, usuario: str, nome_usuario: str, robo: BotBase
+    mensagem: str,
+    usuario: Usuario,
+    telefone: str,
+    nome_usuario: str,
+    robo: BotBase,
+    uow: UnitOfWork,
 ):
     try:
-        resposta = await bot.processar_mensagem(mensagem, nome_usuario=nome_usuario)
-        return robo.responder(resposta, usuario)  # type: ignore[arg-type]
+        resposta = await bot.processar_mensagem(
+            mensagem, nome_usuario=nome_usuario, usuario=usuario, uow=uow
+        )
+        return robo.responder(resposta, telefone)
 
     except ComandoDesconhecido:
         try:
             classifier = ClassificadorTexto()
             tipo, _ = classifier.classificar_mensagem(mensagem)
             if tipo == "debito" or tipo == "credito":
-                resposta = comando_criar_transacao(tipo.upper(), mensagem)
+                resposta = comando_criar_transacao(usuario, tipo.upper(), mensagem, uow)
 
-                return robo.responder(resposta, usuario)
+                return robo.responder(resposta, telefone)
         except NaoEhTransacao:
-            robo.responder("Não entendi sua mensagem 🫤", usuario)
-            robo.responder(bot.get_help(), usuario)
-
-
-def comando_criar_transacao(tipo: str, mensagem: str) -> str:
-    parser = ConstrutorTransacao(acao=TipoTransacao[tipo])
-    acao = "pagamento" if tipo == "debito" else "recebimento"
-    transacao_comando = parser.parse_message(mensagem)
-    resposta = f"Entendi! Houve um {acao} de {Real(transacao_comando.valor)} no dia {transacao_comando.data_formatada} na categoria *{transacao_comando.categoria}*."
-
-    return resposta
+            robo.responder("Não entendi sua mensagem 🫤", telefone)
+            robo.responder(bot.ajuda(), telefone)
