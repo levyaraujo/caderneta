0da233f0b0c24fee947aa1477d6de448cb70f33e
Levy <lev0x79@gmail.com>
2024-11-02 23:40:23 -0300

    implementa onboard de usuário pelo WhatsApp e pequenas melhorias

--- src/dominio/usuario/resources.py
+++ src/dominio/usuario/resources.py
@@ -1,15 +1,10 @@
-from fastapi import APIRouter, status, Request, HTTPException
-from pydantic import BaseModel, EmailStr
 import logging
 
-from src.dominio.usuario.entidade import UsuarioModel, UsuarioResposta
+from fastapi import APIRouter, status, HTTPException
+
+from src.dominio.usuario.entidade import UsuarioModel
 from src.dominio.usuario.exceptions import UsuarioJaExiste, ErroAoCriarUsuario
 from src.dominio.usuario.services import criar_usuario
-from src.infra.database.connection import (
-    GET_DEFAULT_SESSION_CONTEXT,
-    GET_DEFAULT_SESSION,
-)
-from src.infra.database.uow import UnitOfWork
 
 UsuarioRouter = APIRouter(prefix="/usuario", tags=["usuario"])
 
@@ -17,12 +12,10 @@ logger = logging.getLogger("usuario_resources")
 
 
 @UsuarioRouter.post("", status_code=status.HTTP_201_CREATED)
-def usuario_onboard(usuario: UsuarioModel) -> UsuarioResposta:
-    uow = UnitOfWork(session_factory=GET_DEFAULT_SESSION)
+def usuario_onboard(usuario: UsuarioModel):
     try:
-        usuario_criado = criar_usuario(usuario, uow=uow)
-        resposta = UsuarioResposta(**usuario_criado.model_dump())
-        return resposta
+        usuario_criado = criar_usuario(usuario)
+        return {"message": "Usuario cadastrado com sucesso!"}
     except UsuarioJaExiste as erro:
         logger.info(f"Usuário {usuario.email} já existe no sistema")
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(erro))
