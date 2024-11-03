db865af51de1dab4e50be468c7cd2e31db4d89d6
Levy Araujo <lev0x79@gmail.com>
2024-11-03 12:21:11 -0300

    adiciona envs CI e adiciona Dockerfile

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
 from dotenv import load_dotenv
+import nltk
 
 load_dotenv("../.env.test")
+
+nltk.download("punkt", quiet=True)
+nltk.download("stopwords", quiet=True)
+nltk.download("wordnet", quiet=True)
+nltk.download("punkt_tab", quiet=True)
