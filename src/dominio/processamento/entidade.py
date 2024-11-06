300d798103d8fd194ffb7a195ef638fcb57f8432
Levy <lev0x79@gmail.com>
2024-11-06 01:30:16 -0300

    adiciona download nltk

--- src/dominio/processamento/entidade.py
+++ src/dominio/processamento/entidade.py
@@ -32,6 +32,7 @@ logger = logging.getLogger("TextClassifier")
 nltk.download("punkt", quiet=True)
 nltk.download("stopwords", quiet=True)
 nltk.download("wordnet", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 
 
 class ClassificadorTexto:
