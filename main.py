8347845b0c79cee69517027cf46492d347749aa7
Levy Araujo <lev0x79@gmail.com>
2024-11-08 00:25:17 -0300

    Adiciona Sentry ao Projeto e Pequenas Melhorias (#9)
    * adiciona sentry ao projeto
    
    * pequenas melhorias no gerenciamento de ambientes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import sentry_sdk
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
@@ -6,15 +8,29 @@ from starlette.staticfiles import StaticFiles
 from src.dominio.bot.resources import BotRouter
 from src.dominio.usuario.resources import UsuarioRouter
 from src.infra.middlewares.onboard import UserOnboardMiddleware
-import os
 
 BUCKET = os.getenv("BUCKET")
+import dotenv
 
-origins = ["https://caderneta.tunn.dev", "http://localhost", "http://localhost:5173", "http://44.202.71.36"]
+dotenv.load_dotenv(".env")
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=BUCKET), name="static")
+
+sentry_sdk.init(
+    dsn="https://9e11244046c4b957853cc51bd10a478b@o4508257512521728.ingest.us.sentry.io/4508257514618880",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=0.5,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
+
+# app.mount("/static", StaticFiles(directory=BUCKET), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,6 +44,11 @@ app.add_middleware(UserOnboardMiddleware)
 
 app.include_router(BotRouter)
 app.include_router(UsuarioRouter)
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 
 @app.middleware("http")
