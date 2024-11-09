376ffa7e9b4bca1677d51f331d5d8e27ebfe7226
Levy <lev0x79@gmail.com>
2024-11-09 01:34:47 -0300

    implementa middleware whatsapp

--- src/infra/middlewares/twilio.py
+++ src/infra/middlewares/twilio.py
@@ -1,52 +1,64 @@
 import logging
-import os
 import traceback
+from typing import Coroutine, Any, Callable
 
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
 from twilio.twiml.messaging_response import MessagingResponse
 
-from src.dominio.usuario.onboard import OnboardingHandler, OnboardingState
+from src.dominio.usuario.exceptions import UsuarioJaExiste
+from src.dominio.usuario.onboard import OnboardingHandler
 from src.dominio.usuario.repo import RepoUsuarioLeitura
 from src.infra.database.connection import get_session
 from src.infra.database.uow import UnitOfWork
-
-FRONT = os.getenv("FRONT")
+from src.utils.formatos import formatar_telefone
 
 logger = logging.getLogger("onboard_middleware")
 
 
-class UserOnboardMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+class TwilioOnboardMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware para processar requisições de onboarding de usuários.
+    """
+
+    async def dispatch(self, request: Request, call_next: Callable) -> Coroutine[Any, Any, Response] | Response:
         try:
             dados = await request.form()
             request.state.form_data = dados
 
-            if "/bot" not in request.url.path:
+            request.state.usuario = None
+
+            if "/bot/twilio" not in request.url.path:
                 return await call_next(request)
 
-            usuario_telefone = dados.get("From", "").replace("whatsapp:+", "")
-            usuario_telefone = usuario_telefone[:4] + "9" + usuario_telefone[4:]
+            telefone = dados.get("From", "").replace("whatsapp:+", "")
+            telefone = formatar_telefone(telefone)
             mensagem = dados.get("Body", "")
+            nome_usuario = dados.get("ProfileName", "")
 
             repo = RepoUsuarioLeitura(session=get_session())
 
-            user = repo.buscar_por_telefone(usuario_telefone)
+            usuario = repo.buscar_por_telefone(telefone)
 
-            if not user:
+            if not usuario:
                 uow = UnitOfWork(session_factory=get_session)
                 onboard = OnboardingHandler(uow=uow)
-                resposta = onboard.handle_message(usuario_telefone, mensagem)
+                resposta = onboard.handle_message(telefone, mensagem, nome_usuario)
                 twiml = MessagingResponse()
                 twiml.message(resposta)
                 return Response(content=str(twiml), media_type="application/xml")
 
-            request.state.user = user
+            request.state.usuario = usuario
             return await call_next(request)
 
+        except UsuarioJaExiste:
+            twiml = MessagingResponse()
+            twiml.message("Desculpe, mas já existe um usuário com o mesmo email no sistema.")
+            return Response(content=str(twiml), media_type="application/xml")
+
         except Exception as e:
-            logger.error(traceback.format_exc())
+            traceback.print_exc()
             print(f"Error in middleware: {str(e)}")
             twiml = MessagingResponse()
             twiml.message(
---
+++ src/infra/middlewares/whatsapp.py
@@ -0,0 +1,65 @@
+import json
+import logging
+import traceback
+from typing import Coroutine, Any, Callable
+
+from starlette.exceptions import HTTPException
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
+from twilio.twiml.messaging_response import MessagingResponse
+
+from src.dominio.bot.entidade import WhatsAppBot
+from src.dominio.usuario.onboard import OnboardingHandler
+from src.dominio.usuario.repo import RepoUsuarioLeitura
+from src.infra.database.connection import get_session
+from src.infra.database.uow import UnitOfWork
+from src.utils.whatsapp_api import parse_whatsapp_payload
+
+logger = logging.getLogger("onboard_middleware")
+
+
+class WhatsAppOnboardMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware para processar requisições de onboarding de usuários.
+    """
+
+    async def dispatch(self, request: Request, call_next: Callable) -> Coroutine[Any, Any, Response] | Response:
+        dados = await request.json()
+        dados = parse_whatsapp_payload(dados)
+        try:
+            request.state.form_data = dados
+
+            request.state.usuario = None
+
+            if "/bot/whatsapp" not in request.url.path or not dados:
+                return await call_next(request)
+
+            repo = RepoUsuarioLeitura(session=get_session())
+            usuario = repo.buscar_por_telefone(dados.telefone)
+
+            if not usuario:
+                uow = UnitOfWork(session_factory=get_session)
+                bot = WhatsAppBot()
+                onboard = OnboardingHandler(uow=uow)
+                pergunta_onboard = onboard.handle_message(dados.telefone, dados.mensagem, dados.nome)
+                resposta = bot.responder(pergunta_onboard, dados.telefone)
+                return JSONResponse(content=resposta)
+
+            request.state.usuario = usuario
+            return await call_next(request)
+
+        except Exception:
+            traceback.print_exc()
+            dados = await request.json()
+            dados = parse_whatsapp_payload(dados)
+            bot = WhatsAppBot()
+
+            if dados:
+                resposta = bot.responder(
+                    "Desculpe, ocorreu um erro ao processar sua mensagem.\nPor favor, tente novamente mais tarde.",
+                    dados.telefone,
+                )
+                return JSONResponse(content=resposta)
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao processar mensagem")
