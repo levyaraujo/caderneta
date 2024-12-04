99be4431f36729ab3b5394aef81f47a47e71f039
Levy Araujo <lev0x79@gmail.com>
2024-12-03 21:27:41 -0300

    adiciona comando exportar

--- src/utils/uploader.py
+++ src/utils/uploader.py
@@ -4,15 +4,15 @@ import dotenv
 
 dotenv.load_dotenv()
 
-BUCKET = os.getenv("BUCKET")
+BUCKET = os.getenv("BUCKET", "/opt/caderneta/static")
 STATIC_URL = os.getenv("STATIC_URL")
 
 
 class Uploader:
-    def __init__(self):
+    def __init__(self) -> None:
         os.makedirs(BUCKET, exist_ok=True)
 
-    def upload_file(self, object_key: str, arquivo: bytes):
+    def upload_file(self, object_key: str, arquivo: bytes) -> str:
         caminho_completo = os.path.join(BUCKET, object_key)  # noqa
 
         os.makedirs(BUCKET, exist_ok=True)
