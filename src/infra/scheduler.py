7f93eb03a67024870830a0b9050f1f734a5b577d
Levy Araujo <lev0x79@gmail.com>
2024-12-07 15:11:45 -0300

    adiciona log de modelo treinado

--- src/infra/scheduler.py
+++ src/infra/scheduler.py
@@ -1,15 +1,12 @@
-import sys
 from contextlib import asynccontextmanager
 
+import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-import logging
-import pytz
 from fastapi import FastAPI
 
 from src.dominio.processamento.entidade import ClassificadorTexto
 from src.infra.log import setup_logging
-from src.infra.migration import run_migrations
 
 logger = setup_logging()
 
@@ -20,6 +17,8 @@ def treinar_modelo():
     classificador = ClassificadorTexto()
     classificador.treinar_modelo()
     classificador.salvar_modelo()
+
+    logger.info("Modelo treinado com sucesso!")
     return
 
 
